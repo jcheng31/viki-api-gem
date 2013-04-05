0029d99792a3488c990c247d0e1973c35a7795eb
Albert Callarisa Roca <albert@viki.com>
2013-04-05 17:54:24 +0800

    Fix :bomb:

--- spec/viki/contribution_spec.rb
+++ spec/viki/contribution_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe Viki::Contribution, api: true do
   let(:contribution) {
     {
-      id: "100048u",
-      username: "xMaku93",
-      avatar: "http://0.viki.io/u/100048/100048.6dc71ba717339d7eca14c415c1370097b6ac7b36.jpg",
-      segments: 0,
-      subtitles: 10
+      'id' => "100048u",
+      'username' => "xMaku93",
+      'avatar' => "http://0.viki.io/u/100048/100048.6dc71ba717339d7eca14c415c1370097b6ac7b36.jpg",
+      'segments' => 0,
+      'subtitles' => 10
     }}
 
   it "loads container contributions" do
--- spec/viki/notification_spec.rb
+++ spec/viki/notification_spec.rb
@@ -2,31 +2,31 @@ require 'spec_helper'
 
 describe Viki::Notification, api: true do
   it "creates a notification" do
-    stub_api 'notifications.json', Oj.dump({ a: true }), { method: 'post' }
+    stub_api 'notifications.json', Oj.dump({'a' => true}), {:method => 'post'}
     described_class.create(container_id: '1c', content: 'Hi followers') do |response|
-      response.value[:a].should be_true
+      response.value['a'].should be_true
     end
   end
 
   it "fetches notifications" do
-    stub_api 'users/1u/notifications/11501n.json', Oj.dump({ a: true })
+    stub_api 'users/1u/notifications/11501n.json', Oj.dump({'a' => true})
     described_class.fetch(id: "11501n", user_id: '1u') do |response|
-      response.value[:a].should be_true
+      response.value['a'].should be_true
     end
   end
 
   describe "#unread_count" do
     it "fetches with only_count parameter" do
-      stub_api 'users/2u/notifications.json', Oj.dump({ count: 10 }), { unread: 'true', only_count: 'true' }
+      stub_api 'users/2u/notifications.json', Oj.dump({'count' => 10}), {'unread' => 'true', 'only_count' => 'true'}
       described_class.unread_count("2u") do |response|
-        response.value[:count].should == 10
+        response.value['count'].should == 10
       end
     end
   end
   describe "#unread_count_sync" do
     it "synchronously fetches with only_count parameter" do
-      stub_api 'users/2u/notifications.json', Oj.dump({ count: 10 }), { unread: 'true', only_count: 'true' }
-      described_class.unread_count_sync("2u").value.should == { count: 10 }
+      stub_api 'users/2u/notifications.json', Oj.dump({'count' => 10}), {'unread' => 'true', 'only_count' => 'true'}
+      described_class.unread_count_sync("2u").value.should == {'count' => 10}
     end
   end
 end
