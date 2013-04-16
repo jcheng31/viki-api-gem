e4e014bbd0eabdd0904a6b18efe628bfbbe764f3
Fadhli Rahim <fadhli@viki.com>
2013-04-16 10:38:16 +0800

    support for fetching artists casts information

--- spec/viki/artist_spec.rb
+++ spec/viki/artist_spec.rb
@@ -3,4 +3,21 @@ require 'spec_helper'
 describe Viki::Artist, api: true do
   before { stub_api 'artists.json', json_fixture(:artists) }
   it_behaves_like "a container object"
+
+  context "fetches casts" do
+    it do
+      stub_api 'artists/129c/casts.json', '["1a", "2a"]'
+      casts = nil
+      described_class.fetch(artist_id: '129c') { |response| casts = response.value }
+      Viki.run
+      casts.should eq ['1a', '2a']
+    end
+    it "using the cast_for class method" do
+      resp = stub
+      described_class.should_receive(:fetch).with(artist_id: '129c').and_yield(resp)
+      Viki::Artist.casts_for('129c') do |res|
+        res.should eq resp
+      end
+    end
+  end
 end
