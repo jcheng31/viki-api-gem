4cff799eae75c5285875994c227f6af7bc891678
Albert Callarisa Roca <albert@viki.com>
2013-04-24 14:51:24 +0800

    Removed single-video subtitle completions url

--- spec/viki/subtitle_completion_spec.rb
+++ spec/viki/subtitle_completion_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe Viki::SubtitleCompletion, api: true do
   it "fetches a single subtitle" do
-    stub_api 'videos/44699v/subtitle_completions.json', "SubtitleCompletion"
-    described_class.fetch(video_id: "44699v") do |response|
+    stub_api 'subtitle_completions.json', "SubtitleCompletion", params: {video_ids: "44699v,123v"}
+    described_class.fetch(video_ids: "44699v,123v") do |response|
       response.value.should == "SubtitleCompletion"
     end
   end
