541c1ffe0d5345250108c0364761e9078dab8ef9
Deepika <pair+deepika@viikii.com>
2013-05-08 14:19:43 +0800

    version update, changes post to put

--- spec/viki/replace_streams_spec.rb
+++ spec/viki/replace_streams_spec.rb
@@ -1,10 +1,10 @@
 require "spec_helper"
 
 describe Viki::ReplaceStream, api: true do
-  describe 'create' do
+  describe 'update' do
     it 'replaces the streams' do
-      stub_api 'replace_streams.json', '[]', method: :post
-      described_class.create("old_video_id" => '123v', "new_video_id" => '145v') do |response|
+      stub_api 'replace_streams.json', '[]', method: :put
+      described_class.update("old_video_id" => '123v', "new_video_id" => '145v') do |response|
         response.error.should be_nil
       end
     end
