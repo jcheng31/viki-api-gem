9874b4181199916214391ed9a759445154bf41b9
Sunjin Lee <pair+sunjin@viki.com>
2013-06-06 10:14:54 +0800

    use named path

--- lib/viki/translation.rb
+++ lib/viki/translation.rb
@@ -1,15 +1,20 @@
 module Viki
   class Translation < Core::Base
-    path 'v4/translations/:type.json'
-    path 'v4/translations/:target_subtitle_id/:preference.json'
-    
+    LIKE = 'like'
+    DISLIKE = 'dislike'
+    RANDOM = 'random'
+
+    path 'v4/translations/random.json', name: RANDOM
+    path 'v4/translations/:target_subtitle_id/like.json', name: LIKE
+    path 'v4/translations/:target_subtitle_id/dislike.json', name: DISLIKE
+
     def self.random(options = {}, &block)
-      self.fetch(options.merge(type: 'random'), &block)
+      self.fetch(options.merge(named_path: RANDOM), &block)
     end
-    
+
     def self.rating(options = {}, &block)
-      preference = options.delete(:like) ? 'like' : 'dislike'
-      self.create(options.merge(preference: preference), &block)
+      preference = options.delete(:like) ? LIKE : DISLIKE
+      self.create(options.merge(named_path: preference), &block)
     end
   end
 end
