6700aa0555705f9982610c40e9f66d4ee0ef2bea
Ali-Akber Saifee <ali@indydevs.org>
2013-06-10 15:48:21 +0800

    Fix for "NameError: uninitialized constant Viki::Logger".

--- lib/viki/logger.rb
+++ lib/viki/logger.rb
@@ -1,19 +1,20 @@
 require 'logger'
+module Viki
+  class Logger < Logger
+    def self.info(text)
+      super(INFO, "[Viki API] #{text}")
+    end
 
-class Viki::Logger < Logger
-  def self.info(text)
-    super(INFO, "[Viki API] #{text}")
-  end
+    def self.error(text)
+      super(ERROR, "[Viki API] #{text}")
+    end
 
-  def self.error(text)
-    super(ERROR, "[Viki API] #{text}")
-  end
+    def self.fatal(text)
+      super(FATAL, "[Viki API] #{text}")
+    end
 
-  def self.fatal(text)
-    super(FATAL, "[Viki API] #{text}")
-  end
-
-  def self.warn(text)
-    super(WARN, "[Viki API] #{text}")
+    def self.warn(text)
+      super(WARN, "[Viki API] #{text}")
+    end
   end
 end
