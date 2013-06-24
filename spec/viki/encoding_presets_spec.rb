c84bc8c8048cea864e6e61094a277bff13b03fd9
Deepika <pair+deepika@viikii.com>
2013-06-24 14:57:21 +0800

    encoding_preset does not video_id

--- spec/viki/encoding_presets_spec.rb
+++ spec/viki/encoding_presets_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe Viki::EncodingPreset, api: true do
   it "fetches a encoding presets" do
-    stub_api 'encoding_presets.json', json_fixture(:encoding_presets) , {:params => {video_id: '1v'}}
-    described_class.fetch(video_id: "1v") do |response|
+    stub_api 'encoding_presets.json', json_fixture(:encoding_presets)
+    described_class.fetch do |response|
       jobs = response.value
       jobs.should be_a_kind_of(Array)
     end
