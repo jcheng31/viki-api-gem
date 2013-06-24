474cee3e7a843ec93368a890795b13995d7f1037
Deepika <pair+deepika@viikii.com>
2013-06-24 10:05:08 +0800

    correct indentation

--- spec/viki/encode_jobs_spec.rb
+++ spec/viki/encode_jobs_spec.rb
@@ -1,20 +1,20 @@
 require 'spec_helper'
 
 describe Viki::EncodeJob, api: true do
-  it "fetches a encode job" do
-    stub_api 'encode_jobs.json', json_fixture(:encode_jobs) , {:params => {video_id: '1v' ,status: 'scheduled'}}
-    described_class.fetch(video_id: "1v", status: 'scheduled') do |response|
-      jobs = response.value
-      jobs.should be_a_kind_of(Array)
-    end
-  end
+	it "fetches a encode job" do
+		stub_api 'encode_jobs.json', json_fixture(:encode_jobs) , {:params => {video_id: '1v' ,status: 'scheduled'}}
+		described_class.fetch(video_id: "1v", status: 'scheduled') do |response|
+			jobs = response.value
+			jobs.should be_a_kind_of(Array)
+		end
+	end
 
-    describe 'create' do
-    it 'creates an encode job' do
-      stub_api 'encode_jobs.json', '[]', method: :post
-      described_class.create("video_id" => '123v') do |response|
-        response.error.should be_nil
-      end
-    end
-  end
+	describe 'create' do
+		it 'creates an encode job' do
+			stub_api 'encode_jobs.json', '[]', method: :post
+			described_class.create("video_id" => '123v') do |response|
+				response.error.should be_nil
+			end
+		end
+	end
 end
--- spec/viki/master_videos_spec.rb
+++ spec/viki/master_videos_spec.rb
@@ -1,23 +1,22 @@
 require "spec_helper"
 
 describe Viki::MasterVideo, api: true do
-  describe 'create' do
-    it 'creates the master video' do
-      stub_api 'master_videos.json', '[]', method: :post
-      described_class.create("url" => "http://example.com/video.flv", "video_id" => '123v') do |response|
-        response.error.should be_nil
-      end
-    end
-  end
+	describe 'create' do
+		it 'creates the master video' do
+			stub_api 'master_videos.json', '[]', method: :post
+			described_class.create("url" => "http://example.com/video.flv", "video_id" => '123v') do |response|
+				response.error.should be_nil
+			end
+		end
+	end
 
-    describe 'fetch' do
-    it 'creates the master video' do
-      stub_api 'master_videos.json', json_fixture(:master_videos) , {:params => {video_id: '123v'}}
-      described_class.fetch("video_id" => '123v') do |response|
-      jobs = response.value
-      jobs.should be_a_kind_of(Hash)
-      end
-    end
-  end
-
+	describe 'fetch' do
+		it 'creates the master video' do
+			stub_api 'master_videos.json', json_fixture(:master_videos) , {:params => {video_id: '123v'}}
+			described_class.fetch("video_id" => '123v') do |response|
+				jobs = response.value
+				jobs.should be eq 
+			end
+		end
+	end
 end
