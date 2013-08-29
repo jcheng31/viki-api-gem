f8bfa969d72163a5a25439f49ae9140804e0e889
Sunjin Lee <pair+sunjin@viki.com>
2013-08-29 10:07:25 +0800

    add end point to apply to be a contributor

--- lib/viki/contribution.rb
+++ lib/viki/contribution.rb
@@ -1,5 +1,12 @@
 class Viki::Contribution < Viki::Core::Base
+  CANDIDATE='mark_as_candidate'
+
   cacheable
   path '/containers/:container_id/contributions'
   path '/users/:user_id/contributions'
+  path '/users/:user_id/contributions/mark_as_candidate', name: CANDIDATE
+
+  def self.mark_as_candidate(options = {}, &block)
+    self.update(options.merge(named_path: CANDIDATE), &block)
+  end
 end
