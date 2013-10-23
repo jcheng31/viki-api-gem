e8fed4c088f4e9233230b216bab6760bf500e504
Fadhli Rahim <fadhli@viki.com>
2013-10-23 10:32:10 +0800

    update referrals path endpoint

--- lib/viki/referral.rb
+++ lib/viki/referral.rb
@@ -1,3 +1,3 @@
-class Viki::Referral < Viki::Core::Base
-  path "/referrals/:user_id"
+class Viki::Referral < Viki::Core::Base  
+  path "/users/:user_id/referrals"
 end
--- spec/viki/referral_spec.rb
+++ spec/viki/referral_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe Viki::Referral do
-  it "fetches plan subscribers by user_id" do
-    stub_api 'referrals/1u.json', Oj.dump({'available_count' => 1, 'used_count' => 1})
+  it "fetches referrals by user_id" do
+    stub_api 'users/1u/referrals.json', Oj.dump({'available_count' => 1, 'used_count' => 1})
+
     described_class.fetch(user_id: "1u") do |response|
       res = response.value
       res["available_count"].should eq 1
