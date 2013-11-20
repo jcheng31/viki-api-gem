16e994b6517e2e631240af638f6d599d8a198cce
Fadhli Rahim <fadhli@viki.com>
2013-11-20 11:03:15 +0800

    revert back to login_id for v4 support

--- lib/viki/session.rb
+++ lib/viki/session.rb
@@ -5,7 +5,7 @@ class Viki::Session < Viki::Core::Base
   path "/sessions/:token"
 
   def self.authenticate(login_id, password, params = {}, &block)
-    body = params.merge({'username' => login_id, 'password' => password, persist: false})
+    body = params.merge({'login_id' => login_id, 'password' => password, persist: false})
     uri = signed_uri({}, body)
     Viki.logger.info "#{self.name} authenticating #{login_id} to the API: #{uri}"
     creator = Viki::Core::Creator.new(uri, body)
