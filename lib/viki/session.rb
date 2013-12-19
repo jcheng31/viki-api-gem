5435ec07c560d97672e8decb71cf2dccc629f6d9
Fadhli Rahim <fadhli@viki.com>
2013-12-19 23:41:12 +0800

    Need to override these methods to ensure the calls are made to the right endpoints.
    This is because viki-api-gem runs as a singleton, once you change the path of an endpoint to another version, the subsequent api call you make will use the previous value of `path` unless you explicitly state the api_version of the next endpoint you're calling.

--- lib/viki/session.rb
+++ lib/viki/session.rb
@@ -1,10 +1,21 @@
 class Viki::Session < Viki::Core::Base
   class AuthenticationError; end
 
-  path "/sessions", api_version: "v5"
-  path "/sessions/:token", api_version: "v5"
+
+  def self.fetch(url_options = {}, &block)
+    path "/sessions", api_version: 'v5'
+    path "/sessions/:token", api_version: 'v5'
+    super
+  end
+
+  def self.fetch_sync(url_options = {}, &block)
+    path "/sessions", api_version: 'v5'
+    path "/sessions/:token", api_version: 'v5'
+    super
+  end
 
   def self.authenticate(login_id, password, params = {}, &block)
+    path "/sessions", api_version: 'v5'
     body = params.merge({'username' => login_id, 'password' => password, persist: false})
     uri = signed_uri({}, body)
     Viki.logger.info "#{self.name} authenticating #{login_id} to the API: #{uri}"
@@ -13,10 +24,21 @@ class Viki::Session < Viki::Core::Base
   end
 
   def self.auth_facebook(token, params = {}, &block)
+    path "/sessions", api_version: 'v5'
     body = params.merge({'facebook_token' => token, persist: false})
     uri = signed_uri({}, body)
     Viki.logger.info "#{self.name} authenticating facebook token to the API: #{uri}"
     creator = Viki::Core::Creator.new(uri, body)
     creator.queue &block
+  end
+
+  def self.update_sync(url_options = {}, body = {})
+    path "/sessions/:token", api_version: 'v4'
+    super
+  end
+
+  def self.update(url_options = {}, body = {}, &block)
+    path "/sessions/:token", api_version: 'v4'
+    super
   end
 end
--- spec/viki/session_spec.rb
+++ spec/viki/session_spec.rb
@@ -58,4 +58,54 @@ describe Viki::Session, api: true do
       end
     end
   end
+
+  describe ".fetch" do
+    it 'calls v5 endpoint' do
+      params = {app: Viki.app_id}
+      stub_request("get", "http://api.dev.viki.io/v5/sessions/token.json")
+        .with(query: hash_including(:sig, :t, params), headers: {'Content-Type'=>'application/json', 'User-Agent'=>'viki'})
+        .to_return(:status => 200, :body => "", :headers => {})
+
+      described_class.fetch(token: 'token') do |res|
+        res.error.should be_nil
+      end
+    end
+  end
+
+  describe ".update" do
+    it 'calls v4 endpoint' do
+      params = {app: Viki.app_id}
+      stub_request("put", "http://api.dev.viki.io/v4/sessions/token.json")
+        .with(query: hash_including(:sig, :t, params), headers: {'Content-Type'=>'application/json', 'User-Agent'=>'viki'})
+        .to_return(:status => 200, :body => "", :headers => {})
+
+      described_class.update(token: 'token') do |res|
+        res.error.should be_nil
+      end
+    end
+  end
+
+  describe ".fetch_sync" do
+    it 'calls v5 endpoint' do
+      params = {app: Viki.app_id}
+      stub_request("get", "http://api.dev.viki.io/v5/sessions/token.json")
+        .with(query: hash_including(:sig, :t, params), headers: {'Content-Type'=>'application/json', 'User-Agent'=>'viki'})
+        .to_return(:status => 200, :body => "", :headers => {})
+
+      res = described_class.fetch_sync(token: 'token')
+      res.error.should be_nil
+    end
+  end
+
+  describe ".update_sync" do
+    it 'calls v4 endpoint' do
+      params = {app: Viki.app_id}
+      stub_request("put", "http://api.dev.viki.io/v4/sessions/token.json")
+        .with(query: hash_including(:sig, :t, params), headers: {'Content-Type'=>'application/json', 'User-Agent'=>'viki'})
+        .to_return(:status => 200, :body => "", :headers => {})
+
+      res = described_class.update_sync(token: 'token')
+      res.error.should be_nil
+    end
+  end
 end
