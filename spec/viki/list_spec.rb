e1e4099817ef773f585e678f433265939aea09a4
Ethan Le <ductoanle@gmail.com>
2014-01-03 15:40:00 +0800

    add spec for list

--- spec/viki/list_spec.rb
+++ spec/viki/list_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Viki::List, api: true do
   it "fetches single list" do
-    stub_api 'lists/1l.json', json_fixture(:list_1l)
+    stub_api 'lists/1l.json', json_fixture(:list_1l), api_version: "v5"
     described_class.fetch(id: "1l") do |response|
       video = response.value
       video.should be_a_kind_of(Array)
