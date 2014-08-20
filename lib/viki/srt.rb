a5d1b2c9db3d3cbec4a19eb33da16b43943cf223
Sunjin Lee <styner32@gmail.com>
2014-08-20 09:26:59 +0800

    fix format on import srt url

--- lib/viki/srt.rb
+++ lib/viki/srt.rb
@@ -1,7 +1,6 @@
 class Viki::Srt < Viki::Core::Base
-  IMPORT_SRT= 'import_srt'
-  path '/videos/:video_id/subtitles/import_srt.json', name: IMPORT_SRT
-  
+  IMPORT_SRT='import_srt'
+  path '/videos/:video_id/subtitles/import_srt', name: IMPORT_SRT
 
   def self.import(options = {}, &block)
     self.create(options.merge(named_path: IMPORT_SRT), &block)
