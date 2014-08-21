b8f5fba4266a6f7dcdb974476ef755c3b0a59510
Sunjin Lee <styner32@gmail.com>
2014-08-21 10:08:24 +0800

    use body to pass srt content and language code on import srt endpoint

--- lib/viki/srt.rb
+++ lib/viki/srt.rb
@@ -2,7 +2,7 @@ class Viki::Srt < Viki::Core::Base
   IMPORT_SRT='import_srt'
   path '/videos/:video_id/subtitles/import_srt', name: IMPORT_SRT
 
-  def self.import(options = {}, &block)
-    self.create(options.merge(named_path: IMPORT_SRT), &block)
+  def self.import(options = {}, body = {}, &block)
+    self.create(options.merge(named_path: IMPORT_SRT), body, &block)
   end
 end
--- spec/viki/srt_spec.rb
+++ spec/viki/srt_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe Viki::Srt, api: true do
   describe "import subtitle" do
    it 'imports a subtitle' do
-      stub = stub_request('post', /.*\/videos\/44699v\/subtitles\/import_srt\.json[^\.]+/)
-      described_class.import({video_id: '44699v',content: "srt text",language: 'en'}) do
+      stub = stub_request('post', /.*\/videos\/44699v\/subtitles\/import_srt\.json[^\.]+/).with(body: {language: 'en', content: 'srt text'})
+      described_class.import({video_id: '44699v'}, {content: "srt text",language: 'en'}) do
       end
     Viki.run
     stub.should have_been_made
