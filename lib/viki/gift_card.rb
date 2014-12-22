6f0034d5bbdf26c0a273366c300aa1c786759486
Ethan Le <ethan@viki.com>
2014-12-22 19:28:53 +0800

    add endpoint for generating gift codes

--- lib/viki/gift_card.rb
+++ lib/viki/gift_card.rb
@@ -1,12 +1,16 @@
 class Viki::GiftCard < Viki::Core::Base
   SEND_EMAIL = 'send_email'
   TYPES = 'types'
+  GENERATE = 'generate'
 
   cacheable
   path "/gift_cards"
+  # should put before 'gift_cards/:gift_code'
+  path "/gift_cards/types", name: TYPES
+  path "/gift_cards/generate", name: GENERATE
+  ###
   path "/gift_cards/:gift_code"
   path "/gift_cards/:gift_code/send_email", name: SEND_EMAIL
-  path "/gift_cards/types", name: TYPES
 
   def self.send_email(options = {}, body={})
     self.create_sync(options.merge(named_path: SEND_EMAIL), body)
@@ -18,5 +22,9 @@ class Viki::GiftCard < Viki::Core::Base
 
   def self.types(options = {})
     self.fetch_sync(options.merge(named_path: TYPES))
+  end
+
+  def self.generate(options = {})
+    self.fetch_sync(options.merge(named_path: GENERATE))
   end
 end
