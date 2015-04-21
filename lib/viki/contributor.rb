5d5a90249b8a1388e68c1d5fc0cd2c40d7cd923d
Ryo Chikazawa <chikathreesix@gmail.com>
2015-04-21 21:17:02 +0800

    Fix contributor to new endpoint

--- lib/viki/contributor.rb
+++ lib/viki/contributor.rb
@@ -1,3 +1,19 @@
 class Viki::Contributor < Viki::Core::Base
-  path "/contributors"
+  CONTRIBUTOR_COUNT = 'count'
+  CONTRIBUTOR_META = 'meta'
+
+  path '/contributors/:user_id/count', name: CONTRIBUTOR_COUNT
+  path '/contributors/:user_id/meta', name: CONTRIBUTOR_META
+
+  def self.fetch_count(options = {}, &block)
+    self.fetch(options.merge(named_path: CONTRIBUTOR_COUNT), &block)
+  end
+
+  def self.fetch_meta(options = {}, &block)
+    self.fetch(options.merge(named_path: CONTRIBUTOR_META), &block)
+  end
+
+  def self.update_meta(options = {}, &block)
+    self.update(options.merge(named_path: CONTRIBUTOR_META), &block)
+  end
 end
