6b7e136dabc32ea62aa89180b405a8edfa85f87e
Ryo Chikazawa <chikathreesix@gmail.com>
2015-04-22 12:22:13 +0800

    Fix specs for contributor endpoints

--- spec/viki/contributor_spec.rb
+++ spec/viki/contributor_spec.rb
@@ -1,15 +1,27 @@
 require 'spec_helper'
 
 describe Viki::Contributor, api: true do
-  it 'fetches contributor\'s info' do
-    stub = stub_request("get", /contributors\/1.json/)
-    described_class.fetch_sync(id: "1")
-    stub.should have_been_made
+  it 'fetches contributor\'s count' do
+    stub_api 'contributors/1/count.json', '{}'
+    described_class.fetch_count(user_id: "1") do |res|
+      res.error.should be_nil
+    end
+    Viki.run
   end
 
-  it 'updates contributor\'s info' do
-    stub = stub_request("put", /contributors\/1.json/)
-    described_class.update_sync(id: '1', latest_tutorial_step: 'welcome')
-    stub.should have_been_made
+  it 'fetches contributor\'s meta' do
+    stub_api 'contributors/1/meta.json', '{}'
+    described_class.fetch_meta(user_id: '1') do |res|
+      res.error.should be_nil
+    end
+    Viki.run
+  end
+
+  it 'updates contributor\'s meta' do
+    stub_api 'contributors/1/meta.json', '{}', method: 'put'
+    described_class.update_meta(user_id: '1', latest_tutorial_step: 'welcome') do |res|
+      res.error.should be_nil
+    end
+    Viki.run
   end
 end
