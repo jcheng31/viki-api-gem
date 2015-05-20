0afd32b8003541854619f9a1b37bbfebc263323a
Aysha Anggraini <aysha@viki.com>
2015-05-20 17:51:41 +0800

    Prepare for 2.2.3 gem release

--- lib/viki/year.rb
+++ lib/viki/year.rb
@@ -1,4 +1,4 @@
-class Viki::Years < Viki::Core::Base
+class Viki::Year < Viki::Core::Base
 	cacheable
 	path '/containers/years', api_version: "v4"
 end
--- spec/viki/years_spec.rb
+++ spec/viki/years_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe Viki::Years, api: true do
+describe Viki::Year, api: true do
   it "fetches years from the API" do
     stub_api 'containers/years.json', json_fixture(:years)
-    Viki::Years.fetch_sync.value.should == ["1920", "1921", "1922", "1923"]
+    Viki::Year.fetch_sync.value.should == ["1920", "1921", "1922", "1923"]
   end
 end
