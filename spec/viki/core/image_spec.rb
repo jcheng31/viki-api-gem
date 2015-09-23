a3d6a976be759856d8b5d56ce194f780fcc4ce5b
Shantanu <gupta.shantanu95@gmail.com>
2015-09-23 16:55:44 +0800

    Fixed image_spec.rb

--- spec/viki/core/image_spec.rb
+++ spec/viki/core/image_spec.rb
@@ -4,18 +4,23 @@ describe Viki::Image, api: true do
   describe 'update' do
     it 'updates the image' do
       stub_api 'images.json', '[]', method: :put
+      stub = stub_request('put', %r{.*/images.json?.*url=145v.*video_id=123v.*})
       described_class.update("video_id" => '123v', "url" => '145v') do |response|
         response.error.should be_nil
       end
+      Viki.run
+      stub.should have_been_made
     end
   end
 
   describe 'fetch' do
-    it 'fetched the images' do
+    it 'fetches the images' do
       stub_api 'images.json', '[]', method: :get
+      stub = stub_request('get', %r{.*/images.json?.*video_id=123v})
       described_class.fetch("video_id" => '123v') do |response|
-        response.error.should be_nil
       end
+      Viki.run
+      stub.should have_been_made
     end
   end
 
