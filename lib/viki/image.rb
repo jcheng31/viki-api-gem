58fd57b4f8e3650edae98ffb4f4d315f2d9950d2
Shantanu <gupta.shantanu95@gmail.com>
2015-09-23 15:27:05 +0800

    Added create endpoint for images

--- lib/viki/image.rb
+++ lib/viki/image.rb
@@ -1,3 +1,4 @@
 class Viki::Image < Viki::Core::Base
+  path "/images/:path/:resource_id/:type", api_version: "v4"
   path "/images"
 end
--- spec/viki/core/image_spec.rb
+++ spec/viki/core/image_spec.rb
@@ -10,12 +10,32 @@ describe Viki::Image, api: true do
     end
   end
 
-    describe 'fetch' do
+  describe 'fetch' do
     it 'fetched the images' do
       stub_api 'images.json', '[]', method: :get
       described_class.fetch("video_id" => '123v') do |response|
         response.error.should be_nil
       end
+    end
+  end
+
+  describe 'create' do
+    it 'creates a new image' do
+      stub = stub_request('post', %r{.*/images/pr/123pr/poster.json.*}).with(body: {
+        file: 'file_data',
+        x: 0,
+        y: 0,
+        source: 'viki'
+      })
+
+      described_class.create({path: 'pr', resource_id: '123pr', type: 'poster'}, {
+        file: 'file_data',
+        x: 0,
+        y: 0,
+        source: 'viki'
+      }) {}
+      Viki.run
+      stub.should have_been_made
     end
   end
 end
