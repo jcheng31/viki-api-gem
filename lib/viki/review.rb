efd3676fa0839bf6c17620eb3403699aea5a8bd8
Swapnil Kumar <swapnil.kumar@viki.com>
2016-01-25 13:00:59 +0800

    Adding review votes endpoint

--- lib/viki/review.rb
+++ lib/viki/review.rb
@@ -1,12 +1,14 @@
 class Viki::Review < Viki::Core::Base
   cacheable
   LANGUAGES = 'languages'
+  VOTE_PATCH = 'vote_patch'
 
   path '/containers/:container_id/reviews'
   path '/reviews'
   path '/reviews/languages', name: LANGUAGES
   path '/users/:user_id/reviews'
   path '/reviews/:review_id'
+  path '/reviews/:review_id/votes', name: VOTE_PATCH
 
   def self.languages(options = {}, &block)
     self.fetch(options.merge(named_path: LANGUAGES), &block)
@@ -18,6 +20,10 @@ class Viki::Review < Viki::Core::Base
 
   def self.update_review(review_id, body = {}, &block)
     self.update({review_id: review_id}, body, &block)
+  end
+
+  def self.update_like(review_id, body = {}, &block)
+    self.patch({review_id: review_id}.merge(named_path: VOTE_PATCH), body, &block)
   end
 
   def self.delete_review(review_id, &block)
